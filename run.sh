0b9a288910eed0467f4f8ee3c9a7d8c7abacc710
Timo Heister <timo.heister@gmail.com>
2018-06-18 20:49:49 -0700

    initial version

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd repo
+git checkout master
+hash=`git show --oneline`
+
+while true; do
+    git pull
+    newhash=`git show --oneline`
+    if [ "$newhash" != "$hash" ] ; then
+	echo "change!"
+	hash=$newhash
+	# on OSX use: afplay "sound.wav" -v 10
+	aplay "sound.wav"
+    fi
+    sleep 5
+done
