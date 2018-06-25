9e0a6d730f71190c9fe52f08df3b59c077430666
Lorraine Hwang <ljhwang@ucdavis.edu>
2018-06-24 22:47:35 -0700

    Pesonalize the merge music. This has not been end to end tested.

--- run.sh
+++ run.sh
@@ -8,10 +8,24 @@ while true; do
     git pull
     newhash=`git show --oneline`
     if [ "$newhash" != "$hash" ] ; then
-	echo "change!"
-	hash=$newhash
+	    echo "change!"
+	    hash=$newhash
+# Get name of author
+      author=`git show --format=%an -s`
+      filename="../$author.wav"
+#remove whitespaces
+      nows_filename="$(echo -e "${filename}" | tr -d '[:space:]')"
+# debug
+#      echo "$author"
+#      echo "$no_filename"
+#      echo "$filename"
+      	# on OSX use: afplay "sound.wav" -v 10
+      if [ -f $nows_filename] ; then
+        aplay "$nows_filename"
+      else
 	# on OSX use: afplay "sound.wav" -v 10
-	aplay "../sound.wav"
+	       aplay "../sound.wav"
+      fi
     fi
     sleep 5
 done
